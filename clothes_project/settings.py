c9d5e7e9bd389163d13882c47252e3d1c509f96b
Anas0Abu0Sultan <anas.b.sn300@example.com>
2023-07-22 23:08:24 +0300

    done to up on server alhamd ll allah

--- clothes_project/settings.py
+++ clothes_project/settings.py
@@ -143,6 +143,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'anas227sultan@gmail.com'  
 EMAIL_HOST_PASSWORD = 'vxlnhqrpzvnbtrhl'  
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
